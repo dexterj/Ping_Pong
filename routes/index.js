234ab2c6a413c9c13f21949f4732dc1db8b99d61
boboluu <drewleestevens@gmail.com>
2014-01-29 16:46:26 -0600

    tourney pages

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,14 @@ var jf = require("jsonfile");
 var file = "data/data.json";
 var slugger = require("slug");
 
+var getTourneyBySlug = function(slug){
+  var entries = jf.readFileSync(file);
+    for(var i=0; i<entries.length; i++){
+        if(entries[i].slug==slug);
+        return entries[i];
+    }
+};
+
 exports.index = function(req, res){
     var entries = jf.readFileSync(file);
     res.render('index', { title: 'Ping Pong', tournaments: entries});
@@ -17,4 +25,10 @@ exports.data = function(req, res){
     entries.push(req.body);
     jf.writeFileSync("data/data.json", entries);
     res.redirect('/');
+};
+
+exports.tourney = function(req, res){
+   var t = getTourneyBySlug(req.params.tourney);
+    res.render('tourney', t);
+
 };
