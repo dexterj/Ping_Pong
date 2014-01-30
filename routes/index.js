f4d897d350ad9e9b8f10674f44357229a1c80124
hansent <thomas.hansen@gmail.com>
2014-01-30 16:28:50 -0600

    create player list

--- routes/index.js
+++ routes/index.js
@@ -4,21 +4,50 @@
  */
 
 var slugger = require("slug");
-var models = require("./models");
+var models = require("../models");
 
 exports.index = function(req, res){
-    whenFound = function (err, Tournaments){
-        res.render('index', { title: 'Ping Pong', tournamentlist: 'Tournaments' });
+    whenFound = function (err, tournaments){
+        res.render('index', { title: 'Ping Pong', tournamentlist: tournaments });
     };
     // callback
     models.Tournament.find (whenFound);  
 };
 
 
+function parsePlayerList(textBoxString){
+  var player_names = [];
+  var lines = textBoxString.split("\n");
+
+  for(var i=0; i< lines.length; i++){
+    var line_items = lines[i].split(',');
+
+    for(var j=0; j< line_items.length; j++){
+      var trimmed_name = line_items[j].trim();
+      if(trimmed_name != ""){
+        player_names.push(trimmed_name);
+      }
+    }
+  }
+
+  var players = [];
+  for (var i=0; i<player_names.length; i++){
+    var player = new models.Player();
+    player.name = player_names[i];
+    console.log("CCCC", player_names[i], player);
+    player.save()
+    players.push(player);
+  }
+
+  return players; 
+};
+
 exports.create_tournament = function(req, res){
-    var WWE = new models.Tournament ( {name: req.body.Tournament});
-    WWE.slug=slugger(WWE.name);
-    
+    var t = new models.Tournament ();
+    t.name = req.body.Tournament;
+    t.slug = slugger(t.name);
+    t.players = parsePlayerList(req.body.players);
+    t.save();    
     res.redirect('/');
 };
 
