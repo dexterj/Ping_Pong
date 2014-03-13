5c4001f1a74e62c5209efac1cc80c69663d3ee44
Dexter <tyrannosaurusdex@gmail.com>
2014-03-13 14:41:01 -0500

    styled
    DIG IT?!

--- public/main.js
+++ public/main.js
@@ -1,4 +1,5 @@
 var show_popup = function(){
-    $("#popup").css("display", "block")
+    $("#popup").css("display", "block");
+    $("#newTournament").hide();
 }
 $("#newTournament").on("click",show_popup);
